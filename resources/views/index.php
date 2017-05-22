c09fe77033ebcbeda0fccb4b0756e5c38ad94d60
Darthpwner <matthewallenlin@gmail.com>
2017-05-22 14:19:52 -0700

    Work on adding bootstrap template

--- resources/views/index.php
+++ resources/views/index.php
@@ -9,6 +9,9 @@
     <title>Upload</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
     <link href="https://www.ietf.org/lib/dt/6.49.0/ptmono/stylesheet.css" rel='stylesheet' type='text/css'>
     <link href="https://www.ietf.org/lib/dt/6.49.0/ptsans/stylesheet.css" rel='stylesheet' type='text/css'>
     <link href="https://www.ietf.org/lib/dt/6.49.0/ptserif/stylesheet.css" rel='stylesheet' type='text/css'>
@@ -600,6 +603,10 @@
       
 <!-- template: /a/www/ietf-datatracker/web/ietf/templates/debug.html -->
 
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
 
 
       <script src="https://www.ietf.org/lib/dt/6.49.0/jquery/jquery.min.js"></script>
